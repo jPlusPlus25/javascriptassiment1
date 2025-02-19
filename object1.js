2f8407f5bdc5caae904b9faadbf77d970451d40e
arshita <arshitapatel077@gmail.com>
2025-02-19 13:27:57 +0530

    Add files via upload

---
+++ object1.js
@@ -0,0 +1,34 @@
+//object is empty 
+obj ={}
+if(Object.keys(obj)){
+    console.log("object is empty")
+}else{
+    console.log("object is not empty")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
