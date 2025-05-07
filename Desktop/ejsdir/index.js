31b9f2603e2949fb748bf79d76bd7f230095eb94
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-05-07 21:25:33 +0530

    added file

--- Desktop/ejsdir/index.js
+++ Desktop/ejsdir/index.js
@@ -1,7 +1,11 @@
 
 const express = require("express");
 const app=express();
-const port= 8080;
+const port= 3000;
 app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 })
+app.use((req,res)=>{
+    console.log(req);
+    console.log("request received");
+});
