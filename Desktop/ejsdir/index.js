6398d5ad797c4a3b7c87eaed19f4a0ab9aad4fb3
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-05-08 15:56:56 +0530

    update file

--- Desktop/ejsdir/index.js
+++ Desktop/ejsdir/index.js
@@ -6,10 +6,19 @@ app.listen(port,()=>{
     console.log(`listening on port ${port}`);
 });
 
-app.use((req,res)=>{
-    // console.log(req);
-    console.log("request received");
-    res.send(
-        "<h1>Fruits</h1> <ul><li>apple</li><li>orange</li></ul>"    
-    );
-});
+// app.use((req,res)=>{
+//     // console.log(req);
+//     console.log("request received");
+//     res.send(
+//         "<h1>Fruits</h1> <ul><li>apple</li><li>orange</li></ul>"    
+//     );
+// });
+app.get("/",(req,res)=>{
+    res.send("hello i am root")
+})
+app.get("/apple",(req,res)=>{
+    res.send("you contacted apple path")
+})
+app.get("/orange",(req,res)=>{
+    res.send("you contacted orange path")
+})
